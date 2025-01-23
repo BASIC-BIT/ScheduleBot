cb398743b5e8c3638b068fe1082f8ddb13d417bc
BASIC <BASIC.BIT.1001@gmail.com>
2025-01-23 12:11:15 -0500

    Direct ECS to public subnet

--- _infra/main.tf
+++ _infra/main.tf
@@ -227,8 +227,9 @@ resource "aws_ecs_service" "main" {
   desired_count = 1
   launch_type = "FARGATE"
   network_configuration {
-    subnets = aws_subnet.private[*].id
+    subnets = aws_subnet.public[*].id
     security_groups = [aws_security_group.ecs.id]
+    assign_public_ip = true
   }
 
   lifecycle {
